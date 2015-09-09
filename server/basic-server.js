b3d0b9c24fc038ec40140aa0a99d55af8d387dc5
tksander <tkepasander@gmail.com>
2015-09-08 18:50:51 -0700

    Moved folder locations to correct file path serving. Partially fixed html serving bug - still mostly broken. Switching to Express tinkering now.

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,34 +1,74 @@
 /* Import node's http module: */
 var http = require("http");
-var requests = require("./request-handler.js");
 var fs = require("fs");
 var url = require("url");
+var path = require("path");
+var requests = require("./request-handler.js");
 var helpers = require("./responseHelpers.js");
 
-
+var basePath = __dirname;
 var port = 3000;
 var ip = "127.0.0.1";
 
+// var streamer = function(request, response) {
+
+//   console.log('streamer log');
+//   var stream = fs.createReadStream(path.join(basePath, "/"));
+//   stream.on('error', function() {
+//     helpers.sendResponse(response, null, 404);
+//   });
+//   stream.pipe(response);
+// };
+
+var reader = function(request, response) {
+
+  filename = path.join(basePath, "/public/index.html");
+
+  // fs.exists(filename, function(exists) {
+  //   if(!exists) {
+  //     console.log('doesn\'t exist');
+  //     helpers.sendResponse(response, null, 404);
+  //   }
+  // });
+
+var headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10,
+  'Content-Type' : "text/html"
+};
+
+  fs.readFile(filename, function(err, file) {
+    if(err) {
+      console.log('err on reader');
+      console.log(filename);
+      helpers.sendResponse(response, null, 404);
+      return;
+    }
+    console.log('some kind of sucess on reader?')
+    response.writeHead(200, headers);
+    response.write(file);
+    response.end();
+  });
+}
+
 var urls = {
-  "/classes/messages" : true,
-  "/classes/room1" : true,
-  "/": true,
+  "/classes/messages" : requests.requestHandler,
+  "/classes/room1" : requests.requestHandler,
+  "/" : reader
 };
 
 var server = http.createServer(function(request, response) {
 
-  var path = url.parse(request.url).pathname;
+  var urlPath = url.parse(request.url).pathname;
 
-  if(urls[path]) {
-
-    requests.requestHandler(request, response);
-    
+  if(urls[urlPath]) {
+    urls[urlPath](request, response, urlPath);
   } else {
     helpers.sendResponse(response, null, 404);
   }
  
-
-
   //   fs.readFile('../client/client/', function(err, data) {
   //   if (err) {
   //     response.writeHe  ad(404);
